bf4289c9122035c97e8cf61c12db1b593ef0f279
Aaron Elijah <aaronzakelijah@googlemail.com>
2017-07-06 17:57:56 +0100

    Initial Commit

---
+++ API Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  API Demo
+//
+//  Created by Aaron Elijah on 06/07/2017.
+//  Copyright © 2017 Aaron Elijah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
